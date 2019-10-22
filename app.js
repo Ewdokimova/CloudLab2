2eed678fb99f94fbca408aeae685209f0b10f41b
Ewdokimova <56859228+Ewdokimova@users.noreply.github.com>
2019-10-22 12:01:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ app.set('views', './views');
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Заголовок', message: 'Тут текст!'});
+    res.render('index', {title: 'Заголовок', message: 'Лабораторная работа 2'});
 });
 
-app.get('/page3', function (req, res) {
-    res.render('page2', {title: 'Страница 2', message: 'Текст второй страницы'});
+app.get('/page2', function (req, res) {
+    res.render('page2', {title: 'Страница 2', message: 'Лаба номер 2'});
 });
 
 app.listen(port, function() {
